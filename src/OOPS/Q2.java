59de05a36d7950aa41ce4c9227301cc03c017eff
Sangit001 <sangit1543@gmail.com>
2026-02-10 23:04:02 +0545

    Abstraction

---
+++ src/OOPS/Q2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+
+abstract class Car{
+    int a = 20;
+    abstract void Color();
+    abstract void Model();
+}
+
+class Detail extends Car{
+
+    void Color(){
+        System.out.println("color is red");
+    }
+    void Model(){
+        System.out.println("Model is BMW");
+    }
+}
+
+class Q2{
+    public static void main(String[] args){
+
+        Car car = new Detail();
+        car.Color();
+        car.Model();
+
+    }
+}
+
+
